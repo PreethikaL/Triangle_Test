32cc3ba21924274505b7e1f4dbcd331dfc881478
PreethikaL <plakshmi@stevens.edu>
2020-02-04 21:35:37 -0500

    TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -26,6 +26,36 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    def testScaleneTriangles(self):
+        self.assertEqual(classifyTriangle(2,3,4),'Scalene','2,3,4 should be scalene')
+
+    def testIsocelesTriangles(self):
+        self.assertEqual(classifyTriangle(5,5,4),'Isoceles','5,5,4 should be isoceles')
+
+    def testZeroInputA(self):
+        self.assertEqual(classifyTriangle(0,7,8),'InvalidInput','0,7,8 A is invalid')
+
+    def testZeroInputB(self):
+        self.assertEqual(classifyTriangle(6,0,8),'InvalidInput','6,0,8 B is invalid')
+
+    def testZeroInputC(self):
+        self.assertEqual(classifyTriangle(6,7,0),'InvalidInput','6,7,0 A is invalid')
+
+    def testBadTypeA(self):
+        self.assertEqual(classifyTriangle('Bad Input A',7,8),'InvalidInput','Bad Input A,7,0 A is invalid type')
+
+    def testBadTypeB(self):
+        self.assertEqual(classifyTriangle(6,7.50,8),'InvalidInput','6,7.50,8 B is invalid type')
+
+    def testBadTypeC(self):
+        self.assertEqual(classifyTriangle(6,7,9.5),'InvalidInput','6,7,9.5 C is invalid type')
+
+    def testNotATriangle(self):
+        self.assertEqual(classifyTriangle(3,4,9),'NotATriangle','3,4,9 is not a triangle')
+
+    def testNotATriangle(self):
+        self.assertEqual(classifyTriangle(3,4,9),'NotATriangle','3,4,9 is not a triangle')
+
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
